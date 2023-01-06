e1376ff13653ecfff05c8c1005dd7629ee8f4571
Techassi <git@techassi.dev>
2023-01-06 01:47:02 +0100

    Remove unused error variants

--- src/error.rs
+++ src/error.rs
@@ -1,23 +1,17 @@
 use rustyline::error::ReadlineError;
 use thiserror::Error;
 
-use crate::ParameterError;
+use crate::ArgError;
 
 pub type ReplResult<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("Cannot add subcommands to a command with parameters")]
-    IllegalSubcommandError,
-
-    #[error("Cannot add parameters to a command with subcommands")]
-    IllegalParameterError,
-
     #[error("Unrecoverable readline error: {0}")]
     EditorError(#[from] ReadlineError),
 
     #[error("Parameter error: {0}")]
-    ParameterError(#[from] ParameterError),
+    ArgError(#[from] ArgError),
 
     #[error("No such command: {0}")]
     NoSuchCommandError(String),
